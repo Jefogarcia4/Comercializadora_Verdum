1beb9aa85549e109a5b9219b08e1122ecd019f85
Yesid214 <yesidbedoya5480@gmail.com>
2024-11-21 12:29:21 -0500

    Implementación de formulario Reporte Movimientos

--- ComercializadoraVerdum/ReporteMovimientos.Designer.cs
+++ ComercializadoraVerdum/ReporteMovimientos.Designer.cs
@@ -30,11 +30,11 @@
         {
             this.dtpFechaMovimiento = new System.Windows.Forms.DateTimePicker();
             this.txtClienteMovimiento = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnFiltrarMovimientos = new System.Windows.Forms.Button();
             this.btnRefrescarMovimientos = new System.Windows.Forms.Button();
             this.btnVolverAnt = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dgvReporteMovimientos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReporteMovimientos)).BeginInit();
             this.SuspendLayout();
             // 
             // dtpFechaMovimiento
@@ -46,63 +46,68 @@
             // 
             // txtClienteMovimiento
             // 
-            this.txtClienteMovimiento.Location = new System.Drawing.Point(218, 6);
+            this.txtClienteMovimiento.Location = new System.Drawing.Point(215, 6);
             this.txtClienteMovimiento.Multiline = true;
             this.txtClienteMovimiento.Name = "txtClienteMovimiento";
-            this.txtClienteMovimiento.Size = new System.Drawing.Size(233, 23);
+            this.txtClienteMovimiento.Size = new System.Drawing.Size(142, 23);
             this.txtClienteMovimiento.TabIndex = 1;
             // 
-            // button1
+            // btnFiltrarMovimientos
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.Control;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(455, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 25);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Filtrar";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnFiltrarMovimientos.BackColor = System.Drawing.SystemColors.Control;
+            this.btnFiltrarMovimientos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFiltrarMovimientos.Location = new System.Drawing.Point(359, 5);
+            this.btnFiltrarMovimientos.Name = "btnFiltrarMovimientos";
+            this.btnFiltrarMovimientos.Size = new System.Drawing.Size(64, 25);
+            this.btnFiltrarMovimientos.TabIndex = 2;
+            this.btnFiltrarMovimientos.Text = "Filtrar";
+            this.btnFiltrarMovimientos.UseVisualStyleBackColor = false;
+            this.btnFiltrarMovimientos.Click += new System.EventHandler(this.btnFiltrarMovimientos_Click);
             // 
             // btnRefrescarMovimientos
             // 
-            this.btnRefrescarMovimientos.Location = new System.Drawing.Point(533, 4);
+            this.btnRefrescarMovimientos.Location = new System.Drawing.Point(424, 5);
             this.btnRefrescarMovimientos.Name = "btnRefrescarMovimientos";
             this.btnRefrescarMovimientos.Size = new System.Drawing.Size(24, 25);
             this.btnRefrescarMovimientos.TabIndex = 7;
             this.btnRefrescarMovimientos.UseVisualStyleBackColor = true;
+            this.btnRefrescarMovimientos.Click += new System.EventHandler(this.btnRefrescarMovimientos_Click);
             // 
             // btnVolverAnt
             // 
-            this.btnVolverAnt.Location = new System.Drawing.Point(560, 4);
+            this.btnVolverAnt.Location = new System.Drawing.Point(450, 5);
             this.btnVolverAnt.Name = "btnVolverAnt";
             this.btnVolverAnt.Size = new System.Drawing.Size(24, 25);
             this.btnVolverAnt.TabIndex = 8;
             this.btnVolverAnt.UseVisualStyleBackColor = true;
+            this.btnVolverAnt.Click += new System.EventHandler(this.btnVolverAnt_Click);
             // 
-            // dataGridView1
+            // dgvReporteMovimientos
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 32);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(572, 404);
-            this.dataGridView1.TabIndex = 9;
+            this.dgvReporteMovimientos.AllowUserToAddRows = false;
+            this.dgvReporteMovimientos.AllowUserToDeleteRows = false;
+            this.dgvReporteMovimientos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReporteMovimientos.Location = new System.Drawing.Point(12, 32);
+            this.dgvReporteMovimientos.Name = "dgvReporteMovimientos";
+            this.dgvReporteMovimientos.ReadOnly = true;
+            this.dgvReporteMovimientos.Size = new System.Drawing.Size(461, 404);
+            this.dgvReporteMovimientos.TabIndex = 9;
             // 
             // ReporteMovimientos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(588, 442);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(478, 442);
+            this.Controls.Add(this.dgvReporteMovimientos);
             this.Controls.Add(this.btnVolverAnt);
             this.Controls.Add(this.btnRefrescarMovimientos);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnFiltrarMovimientos);
             this.Controls.Add(this.txtClienteMovimiento);
             this.Controls.Add(this.dtpFechaMovimiento);
             this.Name = "ReporteMovimientos";
             this.Text = "Informe de Movimientos";
             this.Load += new System.EventHandler(this.ReporteMovimientos_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReporteMovimientos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,9 +117,9 @@
 
         private System.Windows.Forms.DateTimePicker dtpFechaMovimiento;
         private System.Windows.Forms.TextBox txtClienteMovimiento;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnFiltrarMovimientos;
         private System.Windows.Forms.Button btnRefrescarMovimientos;
         private System.Windows.Forms.Button btnVolverAnt;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvReporteMovimientos;
     }
 }
--- ComercializadoraVerdum/ReporteMovimientos.cs
+++ ComercializadoraVerdum/ReporteMovimientos.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -13,19 +16,35 @@ namespace ComercializadoraVerdum
 {
     public partial class ReporteMovimientos : Form
     {
+        private OleDbConnection connection;
+        private IConfigurationRoot configuration;
+        private DataTable movimientos;
         public ReporteMovimientos()
         {
             this.Icon = new Icon("Images/verdum-logo-icono.ico");
             InitializeComponent();
+            InitializeDatabaseConnection();
             SetButtonImageFromUrl();
-
+            ToolTip toolTip = new ToolTip();
+            toolTip.SetToolTip(btnVolverAnt, "Cerrar y volver al formulario anterior");
+            toolTip.SetToolTip(btnRefrescarMovimientos, "Refrescar información");
         }
-
-        private void ReporteMovimientos_Load(object sender, EventArgs e)
+        private void InitializeDatabaseConnection()
         {
 
-        }
+            var builder = new ConfigurationBuilder()
+                       .SetBasePath(Directory.GetCurrentDirectory())
+                       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
+            configuration = builder.Build();
+
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            connection = new OleDbConnection(connectionString);
+        }
+        private void ReporteMovimientos_Load(object sender, EventArgs e)
+        {
+            CargarMovimientos();
+        }
         public void SetButtonImageFromUrl()
         {
             try
@@ -57,6 +76,88 @@ namespace ComercializadoraVerdum
             {
                 MessageBox.Show("Ocurrió un error al descargar la imagen: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+        private void CargarMovimientos()
+        {
+            try
+            {
+                connection.Open();
+                string consulta = @"
+            SELECT v.NombreCliente, mv.TipoPago, mv.ValorAbono, mv.Fecha
+            FROM MovimientoVentas mv
+            INNER JOIN Ventas v ON mv.VentaId = v.VentaId
+            ORDER BY mv.Fecha DESC";
+
+                OleDbDataAdapter adapter = new OleDbDataAdapter(consulta, connection);
+                movimientos = new DataTable();
+                adapter.Fill(movimientos);
+
+                dgvReporteMovimientos.DataSource = movimientos;
+
+                if (dgvReporteMovimientos.Columns["ValorAbono"] != null)
+                {
+                    dgvReporteMovimientos.Columns["ValorAbono"].DefaultCellStyle.Format = "N2";
+                    dgvReporteMovimientos.Columns["ValorAbono"].DefaultCellStyle.FormatProvider =
+                        System.Globalization.CultureInfo.CreateSpecificCulture("es-CO");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los movimientos: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void btnFiltrarMovimientos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filtroCliente = txtClienteMovimiento.Text.Trim();
+                DateTime filtroFecha = dtpFechaMovimiento.Value.Date;
+
+                DataView vistaFiltrada = new DataView(movimientos);
+
+                string filtro = "";
+
+                if (dtpFechaMovimiento.Checked) 
+                {
+                    filtro = $"Fecha = #{filtroFecha:MM/dd/yyyy}#";
+                }
+
+                if (!string.IsNullOrEmpty(filtroCliente))
+                {
+                    if (!string.IsNullOrEmpty(filtro))
+                    {
+                        filtro += " AND ";
+                    }
+                    filtro += $"NombreCliente = '{filtroCliente}'";
+                }
+
+                vistaFiltrada.RowFilter = filtro;
+
+                dgvReporteMovimientos.DataSource = vistaFiltrada;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al filtrar los movimientos: " + ex.Message);
+            }
+        }
+
+
+
+        private void btnRefrescarMovimientos_Click(object sender, EventArgs e)
+        {
+            CargarMovimientos();
+            txtClienteMovimiento.Text = "";
+            dtpFechaMovimiento.Value = DateTime.Now;
+        }
+
+        private void btnVolverAnt_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
